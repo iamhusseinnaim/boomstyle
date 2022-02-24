ddc47598e08d1bf9a41305c46aaa45be289c6561
hussein <phusseinnaim@gmail.com>
2022-02-24 07:05:02 +0300

    new style for categories page

--- js/plugins/countdown.js
+++ js/plugins/countdown.js
@@ -22,5 +22,42 @@ const countDown = (id)=>{
         setInterval(() => {
             countDown(id)
         }, 1000);
+}
 
+
+class Countdown{
+    constructor(id){
+        this.id = id
+        
+        setInterval(() => {
+            this.run()
+        }, 100);
+        
+    }
+
+    run(){
+
+        var id = this.id
+        var expiryDate = new Date($(id).data('expiry-date')).getTime()
+        var currentTime = new Date().getTime()
+        var gap = expiryDate - currentTime
+        var second = 1000
+        var minute = second * 60
+        var hour = minute * 60
+        var day = hour * 24
+    
+        // calculate date 
+        var dayText = Math.floor(gap / day)
+        var hourText = Math.floor((gap % day) / hour)
+        var minuteText = Math.floor((gap % hour) / minute)
+        var secondText = Math.floor((gap % minute) / second)
+    
+    
+            $(id+' .day ').text(dayText);
+            $(id+" .hour ").text(hourText);
+            $(id+" .minute ").text(minuteText)
+            $(id+' .second ').text(secondText)
+    
+
+    }
 }
