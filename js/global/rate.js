afb0009988ca6c5d2f7bca42e41288ae7e9dbd09
hussein <phusseinnaim@gmail.com>
2022-03-04 05:58:29 +0300

    search bar

--- js/global/rate.js
+++ js/global/rate.js
@@ -1,15 +1,24 @@
 $(document).ready(function () {
     
     $('.rate-star').click(function () {
-        $(this).addClass('fi-sr-star');
-        $(this).prevAll().addClass('fi-sr-star');
-        var input = $('[name=rate]')
+        var hasclass = $(this).hasClass('fi-sr-star');
+
+        if(hasclass){
+            $(this).nextAll().removeClass("fi-sr-star")
+        }else{
+            $(this).addClass('fi-sr-star');
+            $(this).prevAll().addClass('fi-sr-star');
+        }
+        var input = $('[name=value]')
         input.val($(this).data('value'))
     });
 
+
+
+
     $('.remove-rate').click(function (e) { 
         e.preventDefault();
-        var input = $("[name=rate]")
+        var input = $("[name=value]")
         var stars = $(".rate-star")
         input.val('')
         stars.removeClass('fi-sr-star');
