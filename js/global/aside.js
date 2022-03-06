f6b0b6889df5ad9ea798464e2929c546bf7c2387
hussein <phusseinnaim@gmail.com>
2022-03-06 10:10:22 +0300

    fix filter blur effect

--- js/global/aside.js
+++ js/global/aside.js
@@ -4,5 +4,4 @@
 $('.show-settings-button').click(function (e) { 
     e.preventDefault();
     $('.categories-settings').toggleClass('active-settings');
-    $('.content').toggleClass('blur');
   });
